1013d7dd99fd879f46fca723ea9cdfacac145b77
Shahrukh Syed <shah_rukh95@hotmail.com>
2018-10-11 14:55:08 -0400

    Initial Commit

---
+++ HCI4300/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HCI4300
+//
+//  Created by Syed Ahmed on 2018-10-11.
+//  Copyright © 2018 Syed Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
